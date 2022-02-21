b13474acbceda90ea62e81790e9d451d5ed8579f
zakharsazanovets@mail.ru <zakharsazanovets@mail.ru>
2022-02-21 11:01:51 +0300

    create new tappable heart button

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
 import 'circle_image.dart';
 
-class AuthorCard extends StatelessWidget {
+class AuthorCard extends StatefulWidget {
   final String authorName;
   final String title;
   final ImageProvider imageProvider;
@@ -15,6 +15,13 @@ class AuthorCard extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<AuthorCard> createState() => _AuthorCardState();
+}
+
+class _AuthorCardState extends State<AuthorCard> {
+  bool _isFavourited = false;
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(16),
@@ -24,7 +31,7 @@ class AuthorCard extends StatelessWidget {
           Row(
             children: [
               CircleImage(
-                imageProvider: imageProvider,
+                imageProvider: widget.imageProvider,
                 imageRadius: 20,
               ),
               const SizedBox(
@@ -34,11 +41,11 @@ class AuthorCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    authorName,
+                    widget.authorName,
                     style: FooderlichTheme.darkTextTheme.headline2,
                   ),
                   Text(
-                    title,
+                    widget.title,
                     style: FooderlichTheme.darkTextTheme.headline3,
                   ),
                 ],
@@ -47,12 +54,13 @@ class AuthorCard extends StatelessWidget {
           ),
           IconButton(
             onPressed: () {
-              const snackBar = SnackBar(content: Text('Favourite pressed'));
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              setState(() {
+                _isFavourited = !_isFavourited;
+              });
             },
-            icon: const Icon(Icons.favorite_border),
+            icon: Icon(_isFavourited ? Icons.favorite : Icons.favorite_border),
             iconSize: 30,
-            color:  Colors.grey[400],
+            color: Colors.grey[400],
           )
         ],
       ),
